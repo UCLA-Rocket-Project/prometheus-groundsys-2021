fadb46aa27c5b7b6c026dfd195f91c2f0c03a45d
Harrison Cassar <harrison.cassar@gmail.com>
2021-05-01 05:06:31 -0700

    Add calibration values and remove line endings. -Calibration values integrated to pad-side. -Remove line endings generated from operating on Windows system.

--- daq/BunkerDAQ/BunkerDAQ.ino
+++ daq/BunkerDAQ/BunkerDAQ.ino
@@ -13,8 +13,9 @@
 // init Software Serial connection from pad
 SoftwareSerial from_pad_connection(3, 2); // RX, TX
 
-// init `float` variable used as buffer for PT1 data
-float data = -1;
+// init `float` variable used as buffer for PT1 and PT2 data
+float data1 = -1;
+float data2 = -1;
 
 void setup()
 {
@@ -30,16 +31,20 @@ void setup()
 void loop()
 {
   // clear data buffers
-  data = -1;
+  data1 = -1;
+  data2 = -1;
 
   // only attempt to read data from connection if data is available and the first byte indicates the start of a packet we expect ('s')
   if (from_pad_connection.available()) // && from_pad_connection.read() == 's'
   {
     // read and parse float value transmitted (representing PT data)
-    data = from_pad_connection.parseFloat();
+    data1 = from_pad_connection.parseFloat();
+    data2 = from_pad_connection.parseFloat();
 
     // display PT data value to Serial monitor
-    Serial.println(data);
+    Serial.print(data1);
+    Serial.print(",");
+    Serial.println(data2);
   }
 
   // delay because we don't expect data transmitting that fast
--- daq/PadDAQ/PadDAQ.ino
+++ daq/PadDAQ/PadDAQ.ino
@@ -5,54 +5,62 @@
  */
 
 // pin numbers
-#define PIN_PT A0
+#define PIN_PT1 A1
+#define PIN_PT2 A2
 
 // configurable parameters
 #define DEBUGGING false
 
 // global constants
-const float PT_OFFSET = 0.996;
-const float PT_SCALE = 0.00408;
+const float PT1_OFFSET = -209.38;
+const float PT1_SCALE = 244.58;
+const float PT2_OFFSET = 11.924;
+const float PT2_SCALE = 178.51;
 
 // link necessary libraries
 #include <SoftwareSerial.h>
 
 // init SoftwareSerial connection to bunker
 SoftwareSerial to_bunker_connection(4, 8); // RX, TX
-
+ 
 // init `float` variable used as buffer for PT data
-float pt_data = -1;
+float pt1_data = -1;
+float pt2_data = -1;
   
 void setup()
 {
   // start Serial connections
   if (DEBUGGING)
   {
-      Serial.begin(9600);
+    Serial.begin(9600);
   }
 
   to_bunker_connection.begin(57600);
 }
 
-void loop() {
-
+void loop()
+{
   // clear data buffers
-  pt_data = -1;
+  pt1_data = -1;
+  pt2_data = -1;
 
   // get and convert current PT readings
-  pt_data = get_psi_from_raw_pt_data(analogRead(PIN_PT));
+  pt1_data = get_psi_from_raw_pt_data(analogRead(PIN_PT1), 1);
+  pt2_data = get_psi_from_raw_pt_data(analogRead(PIN_PT2), 2);
 
   // send metadata portion of datapacket
   //to_bunker_connection.write('s'); // indicate start of transmission
   //to_bunker_connection.write(sizeof(pt_data)); // send single byte representing size of data transmitted
 
   // send data portion of datapacket
-  to_bunker_connection.println(pt_data); // send single float of PT1 data
+  to_bunker_connection.println(pt1_data); // send single float of PT1 data
+  to_bunker_connection.println(pt2_data); // send single float of PT1 data
 
   // output to Serial debugging information
   if (DEBUGGING)
   {
-    Serial.println(pt_data);
+    Serial.println(pt1_data);
+    Serial.println(pt2_data);
   }
 
   // delay so we don't sample too fast :)
@@ -70,13 +78,27 @@ void loop() {
  * 
  * Returns: float representing pressure (in PSI) corresponding to PT reading
  */
-float get_psi_from_raw_pt_data(int raw_data)
+float get_psi_from_raw_pt_data(int raw_data, int pt_num)
 {
+  float offset;
+  float scale;
+
+  if (pt_num == 1)
+  {
+    offset = PT1_OFFSET;
+    scale = PT1_SCALE;
+  }
+  else if (pt_num == 2)
+  {
+    offset = PT2_OFFSET;
+    scale = PT2_SCALE;
+  }
+
   // convert to voltage
   float voltage = raw_data * (5.0/1023.0); // arduino-defined conversion from raw analog value to voltage
 
   // convert to psi (consider calibration factors)
-  float psi = (voltage - PT_OFFSET) / PT_SCALE; 
+  float psi = voltage*scale + offset; 
 
   // return value
   return psi;
