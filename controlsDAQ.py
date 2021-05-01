918895209b36a39ca222a25eca67c3aa32359d29
Shilpa Rao <shilpa.padiki.rao@gmail.com>
2021-05-01 00:46:33 -0700

    Update controlsDAQ.py
    plot not showing up, why are all the bugs coming up now 😭

--- controlsDAQ.py
+++ controlsDAQ.py
@@ -20,6 +20,9 @@ from serial import Serial
 #plt.axis([0, 100, 0, 600])
 # If we don't set axis beforehand, the graph will adjust axes as needed, which is great
 
+def valid(string):
+    return True
+
 # WE ALSO NEED A VALIDATION FUNCTION
 def getTime(string):
     
@@ -27,99 +30,81 @@ def getTime(string):
     # if it comes in as hours minutes seconds, AA:BB:CC
     if valid(string):
 
-        firstComPlace = 0;
-            # is it range(0, string.length()?)
-        for x in string.length():
-            if string[x] == ',':
-                firstComPlace = x;
+        #the start of the time string
+        start = 0
+        end = 0
+    
+        for x in range(0, len(string)): #I think the range func goes up by 1, so accessing up to length should work. Possibly length-1?
+            if string[x] == "'":
+                start = x + 1
+            if string[x] == ",":
+                end = x
                 break
-            else:
-                return False
-        
-        for i in string.length():
-            if string[i] == "'":
-                for j in string.length():
-                    if string[j] == string[firstComPlace]:
-                        time = string[i:j]
-                        return validString
-                    else:
-                        return False #BUT we shouldn't need to return false since
-                                     # valid(string) should check for that
-            else:
-                return False
-    else:
-        return False
-        
 
-def getPressure(string, n):
+        return string[start:end]
+
+
+def getPressure(string):
     # b'16:30:22,5.678\r\n'
-    if valid(string):
-# FIX LATER: WE DON'T WANT TO ACCESS LIKE ARRAY. WE WANT TO GET WHATEVER IS
-#PAST b' AND BEFORE COMMA # 1
-         return string[12:16]
 
-def valid(string):
-    return True
+    return string.split(',') #returns an array
+
+
+## Worst case, this is a hard coded algorithm to find everything in between the commas
+##    array = []
+##    
+##    if valid(string):
+##
+##        commaCount = 0
+##        for x in range(0, len(string)):
+##            # Just getting the comma count for now
+##            if string[x] == ',':
+##                commaCount += 1
+##
+##        copy = commaCount
+##        for i in range(0, len(string)):
+##            if string[i] == ',':
+##                copy += 1
+##            if string[i:i+1] != '\r': #because apparently '\' is a special character???
+##                if copy % 2 != 0:
+##                    startIndex = i
+##                else if copy % 2 == 0:
+##                    endIndex = i
+##                    string s = string[startIndex:endIndex]
+##                    array += [s]
+            
+            
 
                     #change address as needed (check Arduino IDE->tools for this)
-ser = serial.Serial("/dev/cu.usbmodem144101", 9600)
+ser = serial.Serial("/dev/cu.usbmodem141301", 9600)
 
-timeInc = 1
 voltage = []
 time = []
-pressure = []
+totalPT = []
 i = 0;
 
-# garbage input that comes at first
-# , [THING] \ = end of string
-# , [THING , = middle parameter IF THERE IS ONE (probably won't be)
-# ' [THING] , = start of string --> time stamp
+while i<2:
+    string = str(ser.readline())
+    time += [getTime(string)]
+    arrayPT = getPressure(string)
+    totalPT += arrayPT
 
-# ONLY GRAPH VALID INPUT
-while i<6:
-    cc=str(ser.readline())
-    pressure += [cc[8:13]]
-    voltage += [cc[2:10]]
-    #print("x = ", cc[2:7])
-    #print("y = ", cc[8:13])
-    getTime(cc)
-    print(cc[2:10])
-    #print(cc)
-    i+=1
-    plt.plot(voltage, pressure, linewidth = 1,
+    # PRODUCE SEPARATE PLOTS
+
+    for i in range(0,len(arrayPT)):
+        if i > 1 & i < len(arrayPT) - 1:
+            place = arrayPT[i]
+            plt.plot(time, place, linewidth = 1,
          marker='o', markerfacecolor='black', markersize=2)
+
+    plt.show()
     plt.pause(0.05)
+            # plot array[i] vs t in plot that it corresponds to
 
-plt.show()
+    i += 1
+    
+##    plt.plot(time, totalPT, linewidth = 1,
+##         marker='o', markerfacecolor='black', markersize=2)
+##    plt.pause(0.05)
 
-# The code below creates a stagnant plot that only shows up after the arduino is done collecting data.
 
-## voltage = []
-## time = []
-## pressure = []
-## i = 0;
-##ser = serial.Serial("/dev/cu.usbmodem141301", 9600)
-##while (i < 5):
-##     cc=str(ser.readline())
-##     voltage += [cc[2:][:-5]]
-##     pressure += [1]
-##     print("I am working")
-##     plt.plot(voltage, pressure, linewidth = 3,
-##         marker='o', markerfacecolor='black', markersize=8)
-##     i += 1
-##    # plt.show()
-##     
-##   #  print(cc[2:][:-5])
-##
-###plt.plot(voltage, pressure, linewidth = 3,
-##        # marker='o', markerfacecolor='black', markersize=8)
-##  
-### X axis
-##plt.xlabel('Voltage')
-##
-### Y axis
-##plt.ylabel('Pressure')
-##  
-##plt.title('Voltage v. Pressure test from ARES PT')
-##  
-##plt.show()
